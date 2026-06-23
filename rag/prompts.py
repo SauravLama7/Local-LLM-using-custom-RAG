15bcd21b9e11615375bd5bb718dc6215f7d5f87f
SauravLama7 <sauravlamacr7@gmail.com>
2026-06-23 14:02:59 +0545

    Fixes made after evaluation to get better result

--- rag/prompts.py
+++ rag/prompts.py
@@ -1,7 +1,7 @@
 SYSTEM_PROMPTS = {
     "qwen2.5:3b": """
 You are a helpful assistant.
-Answer only using context.
+Answer using information provided within the context and do not use outside knowledge.
 Explain answers clearly with reasoning when needed.
 After each fact or sentence cite the source using [1], [2] etc matching the context number but only cite sources that directly answer the question.
 If a retrieved chunk is about a different topic, ignore it.
@@ -9,6 +9,7 @@ If a retrieved chunk is about a different topic, ignore it.
 
     "ministral-3:3b": """
 You are a helpful teaching assistant.
+Answer using information provided within the context and do not use outside knowledge.
 Explain answers clearly with reasoning when needed.
 After each fact or sentence cite the source using [1], [2] etc matching the context numbers but only cite sources that directly answer the question.
 If a retrieved chunk is about a different topic, ignore it.
